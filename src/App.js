ab02cbcb957cdaf4210c4b2326845dedcb7705c5
Gottfried Kleinberger <friedi.frieds@gmail.com>
2022-06-29 23:23:38 -0400

    Test: driver code for player component

--- src/App.js
+++ src/App.js
@@ -2,7 +2,13 @@ import style from "./App.module.scss";
 import WavePlayer from "./lib";
 const { layout } = style;
 
-const track = "/drubs.mp3";
+const track = {
+  url: "/drubs.mp3",
+  albumCover: "img/darkside.png",
+  album: "Darkside of the Moon",
+  artist: "Pink Floyd",
+  trackTitle: "Any Colour You Like",
+};
 
 function App() {
   return (
@@ -15,7 +21,15 @@ function App() {
         left: "calc(50vw - 150px)",
       }}
     >
-      <WavePlayer url={track} />
+      <WavePlayer
+        track={track}
+        handleSkipNext={() =>
+          console.log("next, handled from the tippity toppity")
+        }
+        handleSkipPrevious={() =>
+          console.log("previous, handled from the tippity toppity")
+        }
+      />
     </div>
   );
 }
