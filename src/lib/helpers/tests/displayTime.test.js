0e8121e27c981cf1859a95d36a57be2b801fc698
Gottfried Kleinberger <friedi.frieds@gmail.com>
2022-07-08 10:34:25 -0400

    testSuite: displaytime() tests

--- src/lib/helpers/tests/displayTime.test.js
+++ src/lib/helpers/tests/displayTime.test.js
@@ -1,5 +1,6 @@
 import { displayTime, timeRemaining } from "../index";
 
+// driver code
 const oneSecond = 1;
 const tenSeconds = 10;
 const oneMinute = 60;
@@ -8,3 +9,26 @@ const oneHour = 3600;
 
 displayTime();
 timeRemaining();
+
+test("display the time:one second in correct format", () => {
+  const time = displayTime(oneSecond);
+  expect(time).toEqual("0:00:01");
+});
+test("display the time:ten seconds in correct format", () => {
+  const time = displayTime(tenSeconds);
+  expect(time).toEqual("0:00:10");
+});
+test("display the time:one min in correct format", () => {
+  const time = displayTime(oneMinute);
+  expect(time).toEqual("0:01:00");
+});
+test("display the time: ten min in correct format", () => {
+  const time = displayTime(tenMinutes);
+  expect(time).toEqual("0:10:00");
+});
+test("display the time: one hour in correct format", () => {
+  const time = displayTime(oneHour);
+  expect(time).toEqual("1:00:00");
+});
+
+// --- elapsed tests
