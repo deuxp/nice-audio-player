2f774b79b1b634350e9169e3a633fb9505e6a937
Gottfried Kleinberger <friedi.frieds@gmail.com>
2022-07-08 09:28:04 -0400

    currentTime

---
+++ src/lib/helpers/tests/displayTime.test.js
@@ -0,0 +1,10 @@
+import { displayTime, timeRemaining } from "../index";
+
+const oneSecond = 1;
+const tenSeconds = 10;
+const oneMinute = 60;
+const tenMinutes = 600;
+const oneHour = 3600;
+
+displayTime();
+timeRemaining();
